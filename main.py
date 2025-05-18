d3aec514948834fee0c4f49731a9ea1230f6c1f5
rollout600 <aaronr.rollins@gmail.com>
2025-05-18 03:38:29 -0700

    added greek and hebrew alphabet + added pathing ability

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-file_contents = ""
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
@@ -17,17 +16,28 @@ from stats import sort_dict
 # This function imports the sort_dict function from the stats module.
 
 def main():
-    path_to_file = "books/frankenstein.txt"
+    import sys
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    # This function checks if the correct number of command line arguments are provided. If not, it prints usage instructions and exits.
+    
+    #variables to be set
+    path_to_file = sys.argv[1] 
     file_contents = get_book_text(path_to_file)
     letter_count = char_count(file_contents)
     sorted = sort_dict(letter_count)
+
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path_to_file}")
     print("----------- Word Count ----------")
     print(f"Found {word_count(file_contents)} total words")
     print("--------- Character Count -------")
     for i in range(0, len(sorted)):
-        print(f"{sorted[i]['letter']}: {sorted[i]['num']}")
+        if sorted[i]['num'] == 0:
+            continue
+        else:
+            print(f"{sorted[i]['letter']}: {sorted[i]['num']}")
 # This function defines the main function that will be executed when the script is run.
 
 main()
